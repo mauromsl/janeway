1784e1a6b763bce3fcb9c59f9dffc6613570fa53
Mauro MSL <mauro.msl@outlook.com>
2024-08-21 16:02:19 +0100

    #3967: Adds Topic model

--- src/submission/models.py
+++ src/submission/models.py
@@ -639,6 +639,7 @@ class Article(AbstractLastModifiedModel):
     language = models.CharField(max_length=200, blank=True, null=True, choices=LANGUAGE_CHOICES,
                                 help_text=_('The primary language of the article'))
     section = models.ForeignKey('Section', blank=True, null=True, on_delete=models.SET_NULL)
+    topic = models.ForeignKey('journal.Topic', blank=True, null=True, on_delete=models.SET_NULL)
     license = models.ForeignKey('Licence', blank=True, null=True, on_delete=models.SET_NULL)
     publisher_notes = models.ManyToManyField('PublisherNote', blank=True, null=True, related_name='publisher_notes')
 
